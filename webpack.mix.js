f22bc0a2804d059145c87e308e34325eda2982a8
tack-k <tyool9c@gmail.com>
2021-08-18 09:19:38 +0900

    chore:fontawesome導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,11 +13,14 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ])
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer'),
+        ]
+    })
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
