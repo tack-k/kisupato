e0344cf922d9b4dbd07b35e087dd7f500ca28ff6
tack-k <tyool9c@gmail.com>
2021-08-20 08:38:17 +0900

    feat:Buttonコンポーネントの作成

---
+++ resources/js/Components/Buttons/OutlineButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="text-pink-500 bg-transparent border border-solid border-pink-500 hover:bg-pink-500 hover:text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+        <slot>ボタン</slot>
+    </button>
+
+</template>
+
+<script>
+export default {
+    name: "OutlineButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Buttons/RegularButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+        <slot>ボタン</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "RegularButton"
+}
+</script>
+
+<style scoped>
+
+</style>
