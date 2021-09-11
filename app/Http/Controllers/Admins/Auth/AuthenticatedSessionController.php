ca29f30ad502883fd6fd97926913c9c24d998630
tack-k <tyool9c@gmail.com>
2021-09-11 18:04:45 +0900

    feat:パスワードリセットのurl変更

--- app/Http/Controllers/Admins/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admins/Auth/AuthenticatedSessionController.php
@@ -20,7 +20,7 @@ class AuthenticatedSessionController extends Controller
     public function create()
     {
         return Inertia::render('Admins/Auth/Login', [
-            'canResetPassword' => Route::has('password.request'),
+            'canResetPassword' => Route::has('admin.password.request'),
             'status' => session('status'),
         ]);
     }
