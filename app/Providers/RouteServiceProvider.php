78d02427e4e60af3b51ba7eab7882070e69edebf
tack-k <tyool9c@gmail.com>
2021-09-11 18:02:42 +0900

    feat:リダイレクト先仮設定

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
      public const USER_HOME = '/dashboard';
-     public const ADMIN_HOME = '/admin/dashboard';
+     public const ADMIN_HOME = '/admin';
 
     /**
      * The controller namespace for the application.
