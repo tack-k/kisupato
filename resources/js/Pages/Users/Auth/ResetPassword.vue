ab5cd1c847c95f457fc5f60a1e8f6274d71d9f03
tack-k <tyool9c@gmail.com>
2021-09-17 08:51:01 +0900

    feat:ユーザーパスワード再設定機能作成

--- resources/js/Pages/Users/Auth/ResetPassword.vue
+++ resources/js/Pages/Users/Auth/ResetPassword.vue
@@ -3,23 +3,23 @@
 
     <form @submit.prevent="submit">
         <div>
-            <breeze-label for="email" value="Email" />
+            <breeze-label for="email" value="登録メールアドレス" />
             <breeze-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
         </div>
 
         <div class="mt-4">
-            <breeze-label for="password" value="Password" />
+            <breeze-label for="password" value="パスワード" />
             <breeze-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
         </div>
 
         <div class="mt-4">
-            <breeze-label for="password_confirmation" value="Confirm Password" />
+            <breeze-label for="password_confirmation" value="パスワード（確認）" />
             <breeze-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <breeze-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Reset Password
+                送信
             </breeze-button>
         </div>
     </form>
