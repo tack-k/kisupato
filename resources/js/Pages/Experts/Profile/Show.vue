ff8425cf6c1b03fe5a3bf90544750838b03f183d
tack-k <tyool9c@gmail.com>
2021-11-15 08:03:05 +0900

    feat:プロフィール表示画面、取得データを表示

--- resources/js/Pages/Experts/Profile/Show.vue
+++ resources/js/Pages/Experts/Profile/Show.vue
@@ -3,27 +3,32 @@
         <template #content>
             <section class="mb-10">
                 <div class="flex mb-4">
-                    <h2 class="base-font-m base-font-bold mr-10">【公開中】</h2>
+                    <h2 class="base-font-m base-font-bold mr-10">【{{ options[profile.status].name }}】</h2>
                     <regular-button class="expert-bg-active">編集する</regular-button>
                 </div>
                 <div class="flex flex-col items-center md:flex-row">
                     <div class="flex flex-col items-center mb-8 md:mb-0">
                         <div class="mb-3">
                             <img
-                                class="w-auto mx-auto rounded-full"
-                                src="https://i.pravatar.cc/150?img=31"
+                                class="h-40 w-40 rounded-full object-cover"
+                                :src="PROFILE_PATH + profile.profile_image"
                                 alt=""
                             />
                         </div>
-                        <p>ニックネーム</p>
+                        <p>{{ profile.nickname }}</p>
                     </div>
-                    <p class="md:ml-8">自己紹介文 テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト</p>
+                    <p class="md:ml-8">{{ profile.self_introduction }}</p>
                 </div>
             </section>
 
             <section class="mb-10">
                 <h2 class="base-font-m base-font-bold mb-4">活動タイトル</h2>
-                <p>テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト</p>
+                <p>{{ profile.activity_title }}</p>
+            </section>
+
+            <section class="mb-10">
+                <h2 class="base-font-m base-font-bold mb-4">活動内容</h2>
+                <p>{{ profile.activity_content }}</p>
             </section>
 
             <section class="mb-10">
@@ -32,10 +37,10 @@
                     <!-- main -->
                     <main class="w-full">
                         <div class="grid grid-cols-4 gap-4">
-                            <div v-for="image in 10" :key="image"
+                            <div v-for="(activity_image, index) in profile.activity_images" :key="index"
                                  class="col-span-4 sm:col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center">
                                 <div class="bg-white rounded-lg mt-5">
-                                    <img :src="'/images/sample-image1.jpeg'" class="rounded-md" alt=""/>
+                                    <img :src="ACTIVITY_PATH + activity_image.activity_image" class="rounded-md h-40 object-cover" alt=""/>
                                 </div>
                             </div>
                             <!-- end cols -->
@@ -46,19 +51,12 @@
 
             <section class="mb-10">
                 <h2 class="base-font-m base-font-bold mb-4">提供技術</h2>
-                <div v-for="skill in 3" :key="skill" class="mb-6">
+                <div v-for="(skill, index) in profile.skills" :key="index" class="mb-6">
                     <div class="mb-4">
-                        <h3 class="base-font-s base-font-bold mb-2">タイトル</h3>
-                        <p>テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト</p>
-                    </div>
-                    <div>
-                        <h3 class="base-font-s base-font-bold mb-2">内容</h3>
-                        <p>テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト
-                            テキスト
-                            テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト</p>
+                        <h3 class="base-font-s base-font-bold mb-2">{{ index + 1 }}.{{ skill.skill_title }}</h3>
+                        <p>{{ skill.skill_content }}</p>
                     </div>
                 </div>
-
             </section>
         </template>
     </my-page-layout>
@@ -68,6 +66,8 @@
 import MyPageLayout from "@/Layouts/Experts/MyPageLayout";
 import {Link} from "@inertiajs/inertia-vue3";
 import RegularButton from "@/Components/Buttons/RegularButton";
+import { reactive } from "vue"
+
 
 export default {
     name: "Show",
@@ -80,9 +80,16 @@ export default {
       profile: Object,
     },
     setup(props) {
-        console.log(props.profile)
+        const PROFILE_PATH = '/storage/profile_images/'
+        const ACTIVITY_PATH = '/storage/activity_images/'
+        const options = reactive([
+            {'id': 0, 'name': '公開'},
+            {'id': 1, 'name': '非公開'},
+        ])
         return {
-
+            PROFILE_PATH,
+            ACTIVITY_PATH,
+            options,
         }
     }
 }
