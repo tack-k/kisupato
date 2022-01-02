ee530b10b2cedd96f8a381393c44a3c3ec762143
tack-k <tyool9c@gmail.com>
2022-01-02 09:27:21 +0900

    feat:人材プロフィール画面に肩書選択を追加

--- resources/js/Pages/Experts/Profile/Input.vue
+++ resources/js/Pages/Experts/Profile/Input.vue
@@ -38,7 +38,10 @@
 
                 <section class="mb-10">
                     <LabelRequired class="label" value="肩書"/>
-
+                    <select name="" id="" class="flex items-center border-gray-300 focus:ring-1 focus:ring-indigo-200 focus:ring-opacity-50 shadow-sm py-2 px-3 border w-full sm:w-1/2">
+                        <option value="">選択してください</option>
+                        <option :value="position.id" v-for="(position, index) in positions" :key="index">{{ position.name }}</option>
+                    </select>
                 </section>
 
                 <section class="mb-10">
