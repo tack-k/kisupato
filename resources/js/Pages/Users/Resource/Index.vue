fc4b54647d7b86b0b74d5448657cfcfa5c5e60de
tack-k <tyool9c@gmail.com>
2022-02-11 20:46:22 +0900

    feat:googleMap ApiのMarkerの設置・不要な記述の削除

--- resources/js/Pages/Users/Resource/Index.vue
+++ resources/js/Pages/Users/Resource/Index.vue
@@ -3,13 +3,14 @@
         <template #content>
             <div>
                 <div class="flex">
-                    <div class="py-6">
+                    <div class="py-6 w-1/2">
                         <div class="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
                             <div class="w-1/3 bg-cover" style="background-image: url('https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')">
                             </div>
                             <div class="w-2/3 p-4">
                                 <h1 class="text-gray-900 font-bold text-2xl">Backpack</h1>
-                                <p class="mt-2 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit In odit exercitationem fuga id nam quia</p>
+                                <p class="mt-2 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing
+                                    elit In odit exercitationem fuga id nam quia</p>
                                 <div class="flex item-center mt-2">
                                     <svg class="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
                                         <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
@@ -34,37 +35,27 @@
                                     </div>
                                     <div class="flex item-center justify-between mt-3">
                                         <h1 class="text-gray-700 font-bold text-xl">$220</h1>
-                                        <button class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">Add to Card</button>
+                                        <button class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+                                            Add to Card
+                                        </button>
                                     </div>
                                 </div>
 
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div>
-                    <GoogleMap
-                        ref="mapRef"
-                        :api-key="GOOGLE_MAP_API_KEY"
-                        style="width: 100%; height: 500px"
-                        :center="center"
-                        :zoom="15"
-                    >
-                        <Marker :options="val"/>
-                    </GoogleMap>
-                    <!--                    <GoogleMap-->
-                    <!--                        ref="mapRef"-->
-                    <!--                        :api-key=GOOGLE_MAP_API_KEY-->
-                    <!--                        class="map"-->
-                    <!--                        :center="center"-->
-                    <!--                        :zoom="2"-->
-                    <!--                    >-->
-                    <!--                        <Marker :options="markerOptions" class="rounded-full w-3 h-3"/>-->
-
-                    <!--                    </GoogleMap>-->
-                    <!--                    <label for="lng">Longitude</label>-->
-                    <!--                    <input v-model.number="lng" id="lng" type="number" min="-180" max="180" step="10" />-->
+                    <div class="w-1/2">
+                        <GoogleMap
+                            ref="mapRef"
+                            :api-key="GOOGLE_MAP_API_KEY"
+                            style="width: 100%; height: 500px"
+                            :center="center"
+                            :zoom="9"
+                        >
+                            <Marker :options="markerOption" v-for="(markerOption, index) in markerOptions" :key="index"/>
+                        </GoogleMap>
+                    </div>
                 </div>
             </div>
         </template>
@@ -74,8 +65,10 @@
 <script>
 import Header from "@/Layouts/Users/Header"
 import FullPageLayout from "@/Layouts/Users/FullPageLayout"
-import { GoogleMap, Marker, IControlPosition } from 'vue3-google-map'
-import { ref, computed, watch, onMounted, reactive, watchEffect } from "vue";
+import {GoogleMap, Marker, IControlPosition} from 'vue3-google-map'
+import {ref, computed} from "vue";
+import {commonConst} from "@/Consts/commonConst"
+
 
 export default {
     name: "Index",
@@ -85,86 +78,50 @@ export default {
         Marker,
         GoogleMap,
     },
-    setup() {
+    props: {
+        profiles: Array,
+    },
+    setup(props) {
+        const {profiles} = props;
+        const {PROFILE_PATH} = commonConst;
         const mapRef = ref(null)
         const GOOGLE_MAP_API_KEY = process.env.MIX_GOOGLE_MAP_API_KEY;
-        const center = { lat: 40.689247, lng: -74.044502 }
-        let markerOptions = reactive({});
+        const center = {lat: 36.197910809248135, lng: 138.06393209955368}
+        let initMarkerOptions = ref([]);
 
-       const val = computed(() => {
+        const mapProfiles = profiles.map(profile => {
+            return {
+                url: PROFILE_PATH + profile.profile_image,
+                lat: profile.latitude,
+                lng: profile.longitude,
+            }
+        })
 
-            console.log(mapRef.value?.ready)
+        const markerOptions = computed(() => {
             if (mapRef.value?.ready) {
 
-               return markerOptions = {
-                    position: center,
-                    label: {
-                        text: 'TTTTT',
-                        color: 'red',
-                    },
-                    icon: {
-                        url: '/images/users/profile.png',
-                        scaledSize: mapRef.value.api.Size(15, 15)
-                    },
-                   anchorPoint: mapRef.value.api.Point(100, 100)
-               }
-                console.log(markerOptions)
-                //     title: 'LADY LIBERTY'
+                initMarkerOptions = mapProfiles.map((mapProfile) => {
+                    return {
+                        position: {lat: mapProfile.lat, lng: mapProfile.lng},
+                        icon: {
+                            url: mapProfile.url,
+                            scaledSize: new mapRef.value.api.Size(30, 30),
+                        },
+                    }
+                })
+
             } else {
-                return markerOptions = {}
+                initMarkerOptions = {}
             }
 
-       })
-        // }, {
-        //     flush: 'post'
-        // })
+            return initMarkerOptions
+        })
 
-// onMounted(() => {
-//     console.log(mapRef.value.ready)
-//          markerOptions = {
-//             position: center,
-//             label: {
-//                 text: 'TTTTT',
-//                 color: 'red',
-//             },
-//             icon: {
-//                 url: '/images/users/profile.png',
-//                 scaledSize: mapRef.value.api.Size(30, 30)
-//             },
-//        //     title: 'LADY LIBERTY'
-//         }
-// })
-        // const center = { lat: 0, lng: 0 }
-        //
-        // const _lng = ref(0)
-        // const lng = computed({
-        //     get: () => _lng.value,
-        //     set: v => {
-        //         if (!Number.isFinite(v)) {
-        //             _lng.value = 0
-        //         } else if (v > 180) {
-        //             _lng.value = 180
-        //         } else if (v < -180) {
-        //             _lng.value = -180
-        //         } else {
-        //             _lng.value = v
-        //         }
-        //     },
-        // })
-
-        // watch(lng, () => {
-        //     if (mapRef.value?.ready) {
-        //         console.log(mapRef.value?.ready)
-        //         mapRef.value.map.panTo({ lat: 0, lng: lng.value })
-        //     }
-        // })
         return {
             center,
             GOOGLE_MAP_API_KEY,
             markerOptions,
             mapRef,
-            // lng,
-            val,
 
         }
     }
@@ -172,25 +129,5 @@ export default {
 </script>
 
 <style scoped>
-.map {
-    position: relative;
-    width: 100%;
-    height: 500px;
-}
 
-.map::after {
-    position: absolute;
-    content: '';
-    width: 1px;
-    height: 100%;
-    top: 0;
-    left: 50%;
-    background: red;
-}
-
-input[type='number'] {
-    width: 200px;
-    margin-top: 20px;
-    margin-left: 10px;
-}
 </style>
