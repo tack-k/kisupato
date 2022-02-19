d64f6e2730ead2261ef3bb1f3185e120b3353f82
tack-k <tyool9c@gmail.com>
2022-02-19 12:16:08 +0900

    feat:人材詳細画面のカルーセルの設定

--- resources/js/Components/Cards/ImageCarousel.vue
+++ resources/js/Components/Cards/ImageCarousel.vue
@@ -1,8 +1,10 @@
 <template>
-    <carousel :items-to-show="1" :wrap-around="true">
+    <carousel :settings="settings" :breakpoints="breakpoints" :wrap-around="true">
         <template #slides>
             <slide v-for="(activityImage, index) in activityImages" :key="index">
-                <img :src="ACTIVITY_PATH + activityImage.activity_image" alt="">
+                <div class="mx-2 w-full">
+                    <img :src="ACTIVITY_PATH + activityImage.activity_image" alt="" :class="{'h-80': isManyImage, 'image-height': !isManyImage}" class="object-cover h-80  w-full">
+                </div>
             </slide>
         </template>
 
@@ -15,9 +17,9 @@
 
 <script>
 import 'vue3-carousel/dist/carousel.css';
-import {Carousel, Slide, Pagination, Navigation} from 'vue3-carousel';
-import {toRefs} from "vue";
-import {commonConst} from "@/Consts/commonConst";
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import { reactive, toRefs, ref } from "vue";
+import { commonConst } from "@/Consts/commonConst";
 
 
 export default {
@@ -32,20 +34,62 @@ export default {
         activityImages: Array,
     },
     setup(props) {
-        const {activityImages} = toRefs(props);
-        const {ACTIVITY_PATH} = commonConst;
-        const slideWidth = 100;
+        const { activityImages } = toRefs(props);
+        const { ACTIVITY_PATH } = commonConst;
+        let breakpoints = reactive({})
+        const isManyImage = ref(null)
+
+        const settings = {
+            itemsToShow: 1,
+        }
+
+        const setBreakPoints = () => {
+            if (activityImages.value.length >= 4) {
+                isManyImage.value = true;
+                breakpoints = {
+                    // 640px and up
+                    640: {
+                        itemsToShow: 1.5,
+                    },
+                    // 1024 and up
+                    1024: {
+                        itemsToShow: 2.5,
+                    },
+                }
+            } else {
+                isManyImage.value = false;
+                breakpoints = {
+                    // 640px and up
+                    640: {
+                        itemsToShow: 1,
+                    },
+                    // 1024 and up
+                    1024: {
+                        itemsToShow: 1,
+                    },
+                }
+            }
+        }
+        setBreakPoints();
 
 
         return {
             activityImages,
             ACTIVITY_PATH,
-            slideWidth
+            settings,
+            breakpoints,
+            isManyImage,
         }
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@media screen and (min-width: 640px) {
+    .image-height {
+        height: 40rem;
+    }
+}
+
 
 </style>
--- resources/js/Pages/Users/Resource/Show.vue
+++ resources/js/Pages/Users/Resource/Show.vue
@@ -1,72 +1,75 @@
 <template>
     <full-page-layout>
         <template #content>
-            <div class="container my-0 mx-auto">
-                <div class="max-w-screen-lg sm:px-8 my-0 mx-auto">
-                    <section class="section-common">
-                        <div class="mt-10">
-                            <SearchInput/>
-                        </div>
-                        <h1 class="base-font-l base-font-bold my-20 text-center">{{ profile.activity_title }}</h1>
-                        <ImageCarousel :activityImages="profile.activity_images"/>
-                        <div class="flex max-w-screen-lg flex-col sm:flex-row my-0 mx-auto items-center :items-start mt-8">
-                            <div class="flex w-full sm:w-3/5 flex-col sm:flex-row">
-                                <div class="w-full sm:w-1/4 flex flex-col items-center">
-                                    <div class="mb-5">
-                                        <img :src="PROFILE_PATH + profile.profile_image" alt="" class="w-40 sm:w-24 h-40 sm:h-24 rounded-full">
-                                    </div>
-                                    <p>{{ profile.nickname }}</p>
+            <div class="max-w-screen-lg sm:px-8 my-0 mx-auto">
+            <section class="section-common">
+                    <div class="mt-10">
+                        <SearchInput/>
+                    </div>
+                    <h1 class="base-font-l base-font-bold my-20 text-center">{{ profile.activity_title }}</h1>
+                </section>
+            </div>
+                <ImageCarousel :activityImages="profile.activity_images"/>
+            <div class="max-w-screen-lg sm:px-8 my-0 mx-auto">
+                <section class="section-common">
+                    <div class="flex max-w-screen-lg flex-col sm:flex-row my-0 mx-auto items-center :items-start mt-8">
+                        <div class="flex w-full sm:w-3/5 flex-col sm:flex-row">
+                            <div class="w-full sm:w-1/4 flex flex-col items-center">
+                                <div class="mb-5">
+                                    <img :src="PROFILE_PATH + profile.profile_image" alt="" class="w-40 sm:w-24 h-40 sm:h-24 rounded-full">
                                 </div>
-                                <div class="p-3 w-full sm:w-3/4">
-                                    <span class="base-font-bold mb-5 user-text-white user-bg-active py-0.5 px-1 text-xs rounded">スーパー人材</span>
-                                    <p class="pt-2">{{ profile.self_introduction }}</p>
-                                </div>
+                                <p>{{ profile.nickname }}</p>
                             </div>
-                            <div class="flex justify-center items-center w-full sm:w-2/5">
-                                <regular-button class="px-16 py-4 font-medium">相談する</regular-button>
+                            <div class="p-3 w-full sm:w-3/4">
+                                <span class="base-font-bold mb-5 user-text-white user-bg-active py-0.5 px-1 text-xs rounded">スーパー人材</span>
+                                <p class="pt-2">{{ profile.self_introduction }}</p>
                             </div>
                         </div>
-                    </section>
-                    <section class="section-common px-4 sm:px-0">
-                        <h2 class="section-title">活動内容</h2>
-                        <p>{{ profile.activity_content }}</p>
-                    </section>
-                    <section class="section-common px-4 sm:px-0">
-                        <h2 class="section-title">提供技術</h2>
-                        <div v-for="(skill, index) in profile.skills" :key="index" class="mb-5">
-                            <h3 class="base-font-s base-font-bold mb-2">{{ skill.skill_title }}</h3>
-                            <p>{{ skill.skill_content }}</p>
-                        </div>
-                        <div class="flex justify-center items-center w-full">
+                        <div class="flex justify-center items-center w-full sm:w-2/5">
                             <regular-button class="px-16 py-4 font-medium">相談する</regular-button>
                         </div>
-                    </section>
+                    </div>
+                </section>
+                <section class="section-common px-4 sm:px-0">
+                    <h2 class="section-title">活動内容</h2>
+                    <p>{{ profile.activity_content }}</p>
+                </section>
+                <section class="section-common px-4 sm:px-0">
+                    <h2 class="section-title">提供技術</h2>
+                    <div v-for="(skill, index) in profile.skills" :key="index" class="mb-5">
+                        <h3 class="base-font-s base-font-bold mb-2">{{ skill.skill_title }}</h3>
+                        <p>{{ skill.skill_content }}</p>
+                    </div>
+                    <div class="flex justify-center items-center w-full">
+                        <regular-button class="px-16 py-4 font-medium">相談する</regular-button>
+                    </div>
+                </section>
 
-                    <section class="section-common">
-                        <div class="bg-gray-100 min-h-screen py-32 px-10 ">
-                            <h2 class="section-title">レビュー</h2>
-                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 ">
-                                <template v-for="(value, index) in 6" :key="index">
-                                    <ReviewCard/>
-                                </template>
-                            </div>
-                            <div class="text-right mt-5">
-                                <span class="text-blue-300 user-hover">○件のレビューをすべて表示</span>
-                            </div>
-                        </div>
-                    </section>
-
-                    <section class="section-common px-4 sm:px-0">
-                        <h2 class="section-title">この人材をチェックした人におすすめ</h2>
-                        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
-                            <template v-for="(profile, key) in profiles" :key="key">
-                                <VerticalCard :profile="profile"/>
+                <section class="section-common">
+                    <div class="bg-gray-100 min-h-screen py-32 px-10 ">
+                        <h2 class="section-title">レビュー</h2>
+                        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 ">
+                            <template v-for="(value, index) in 6" :key="index">
+                                <ReviewCard/>
                             </template>
                         </div>
-                    </section>
+                        <div class="text-right mt-5">
+                            <span class="text-blue-300 user-hover">○件のレビューをすべて表示</span>
+                        </div>
+                    </div>
+                </section>
 
-                </div>
+                <section class="section-common px-4 sm:px-0">
+                    <h2 class="section-title">この人材をチェックした人におすすめ</h2>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
+<!--                        <template v-for="(profile, key) in profiles" :key="key">-->
+<!--                            <VerticalCard :profile="profile"/>-->
+<!--                        </template>-->
+                    </div>
+                </section>
+
             </div>
+
         </template>
     </full-page-layout>
 </template>
@@ -77,18 +80,18 @@ import SearchInput from "@/Components/Forms/RoundSearch";
 import ImageCarousel from "@/Components/Cards/ImageCarousel";
 import RegularButton from "@/Components/Buttons/RegularButton";
 import ReviewCard from "@/Components/Cards/ReviewCard";
-import {toRefs} from "vue";
-import {commonConst} from "@/Consts/commonConst";
+import { toRefs } from "vue";
+import { commonConst } from "@/Consts/commonConst";
 
 export default {
     name: "Show",
-    components: {ReviewCard, RegularButton, ImageCarousel, SearchInput, FullPageLayout},
+    components: { ReviewCard, RegularButton, ImageCarousel, SearchInput, FullPageLayout },
     props: {
         profile: Object,
     },
     setup(props) {
-        const {profile} = toRefs(props)
-        const {PROFILE_PATH, ACTIVITY_PATH} = commonConst;
+        const { profile } = toRefs(props)
+        const { PROFILE_PATH, ACTIVITY_PATH } = commonConst;
 
         return {
             profile,
