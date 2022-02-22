9ce94d89fe9207628c98e6fb63f903bc4eb43a75
tack-k <tyool9c@gmail.com>
2022-02-23 08:39:56 +0900

    fix:【U】headerユーザーログアウトのリンクを修正

--- resources/js/Layouts/Users/Header.vue
+++ resources/js/Layouts/Users/Header.vue
@@ -12,7 +12,7 @@
             <div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
                 <ul class="flex flex-col lg:flex-row list-none ml-auto">
                     <li class="nav-item">
-                        <Link :href="route('admin.logout')" method="post" as="button" class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
+                        <Link :href="route('logout')" method="post" as="button" class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
                             ログアウト
                         </Link>
                     </li>
