84f89462fbc3a49a81c8f88b7fa3f9f398b8b32c
tack-k <tyool9c@gmail.com>
2022-03-02 22:42:19 +0900

    feat:【U】headerのアイコンクリックでトップページに遷移

--- resources/js/Layouts/Users/Header.vue
+++ resources/js/Layouts/Users/Header.vue
@@ -2,9 +2,9 @@
     <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 user-bg">
         <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#">
+                <Link class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" :href="route('home')">
                     KissPATO
-                </a>
+                </Link>
                 <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" @click="toggleNavbar">
                     <Fa :icon="faBars" />
                 </button>
