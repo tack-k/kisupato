f9932f20f670c02e8e56aaabde3c8c8bcb674a8f
tack-k <tyool9c@gmail.com>
2022-03-11 08:30:49 +0900

    feat:【U】お気に入り登録がない場合のメッセージを追加

---
+++ resources/js/Components/Messages/FixedMessage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="text-red-500 border border-gray-200 px-6 py-4 rounded relative mb-4 admin-bg-white">
+      <span class="text-xl inline-block mr-5 align-middle">
+        <Fa :icon="faInfoCircle"/>
+      </span>
+        <p class="inline-block align-middle mr-8 base-font-bold mt-1.5">
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+import Fa from 'vue-fa';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
+export default {
+    name: "FixedMessage",
+    components: {
+        Fa
+    },
+    setup() {
+
+        return {
+            faInfoCircle
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Users/Favorite/Index.vue
+++ resources/js/Pages/Users/Favorite/Index.vue
@@ -1,8 +1,9 @@
 <template>
     <my-page-layout>
         <template #content>
-            <h2 class="mb-8 base-font-m base-font-bold">お気に入り一覧</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+            <h2 class="mb-8 pt-8 base-font-m base-font-bold">お気に入り一覧</h2>
+            <FixedMessage v-if="profiles.length === 0">お気に入りの登録がありません。</FixedMessage>
+            <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
                 <template v-for="(profile, key) in profiles" :key="key">
                     <VerticalCardNoFavoriteButton :profile="profile" @emitDeleteFavorite="handleDeleteFavorite"/>
                 </template>
@@ -16,10 +17,11 @@ import MyPageLayout from '@/Layouts/Users/MyPageLayout'
 import { ref, toRefs } from 'vue'
 import { useFavoriteAction } from '@/Composables/useFavoriteAction'
 import VerticalCardNoFavoriteButton from '@/Components/Cards/VerticalCardNoFavoriteButton'
+import FixedMessage from '@/Components/Messages/FixedMessage'
 
 export default {
     name: "Index",
-    components: { VerticalCardNoFavoriteButton, MyPageLayout },
+    components: { FixedMessage, VerticalCardNoFavoriteButton, MyPageLayout },
     props: {
         profiles: Object,
     },
