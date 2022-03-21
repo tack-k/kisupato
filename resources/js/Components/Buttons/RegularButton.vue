da234e84a1967f860d649c1da832528204ac3de6
tack-k <tyool9c@gmail.com>
2022-03-21 15:28:49 +0900

    style:ボタンコンポーネントの不要なマージンを削除

--- resources/js/Components/Buttons/RegularButton.vue
+++ resources/js/Components/Buttons/RegularButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-yellow-500 text-white active:bg-yellow-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" :type="type">
+    <button class="bg-yellow-500 text-white active:bg-yellow-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150" :type="type">
         <slot>ボタン</slot>
     </button>
 </template>
