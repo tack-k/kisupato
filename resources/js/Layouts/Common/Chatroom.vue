c52b6bdd1cdfae10ceffaa9bb19aba61e8f14e3c
tack-k <tyool9c@gmail.com>
2022-04-03 13:56:49 +0900

    refactor:チャットルームのコンポーネント化

---
+++ resources/js/Layouts/Common/Chatroom.vue
@@ -0,0 +1,162 @@
+<template>
+        <div class="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+            <div class="flex items-center space-x-4">
+                <img :src="profilePath + profile.profile_image" alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full">
+                <div class="flex flex-col leading-tight">
+                    <div class="text-2xl mt-1 flex items-center">
+                        <span class="text-gray-700 mr-3">{{ profile.nickname }}</span>
+                        <span class="text-green-500">
+                                  <svg width="10" height="10">
+                                     <circle cx="5" cy="5" r="5" fill="currentColor"></circle>
+                                  </svg>
+                               </span>
+                    </div>
+                    <span class="text-lg text-gray-600">{{ profile.self_introduction }}</span>
+                </div>
+            </div>
+        </div>
+        <div id="messages" class="flex flex-col space-y-4 p-3 overflow-y-scroll scrolling-touch h-screen" ref="messageRef">
+            <div class="chat-message" v-for="(message, index) in messages" :key="index">
+                <div class="flex items-end" :class="{'justify-end': isLogin(message)}">
+                    <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start" :class="{'user-right': isLogin(message)}">
+                        <div>
+                            <span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600" :class="{'user-message': isLogin(message)}">{{ message.message }}</span>
+                        </div>
+                    </div>
+                    <img v-if="!isLogin(message)" :src="profilePath + message.profile_image" alt="My profile" class="w-6 h-6 rounded-full order-1">
+                </div>
+            </div>
+        </div>
+        <div v-show="canMessage" class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+            <div class="relative flex">
+                <input v-model="newMessage" type="text" class="w-full px-6 border-gray-300 border-2 focus:border-gray-300 text-gray-600 bg-gray-200 rounded-full py-3">
+                <button v-show="isInput" @click="submit" type="button" class="inline-flex items-center justify-center rounded-full h-12 w-12 -ml-4 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 transform rotate-90">
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+</template>
+
+<script>
+import { computed, nextTick, onMounted, ref, toRefs, watch } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
+import { commonConst } from '@/Consts/commonConst'
+
+export default {
+    name: "Chatroom",
+    props: {
+        messages: Object,
+        chatroom: Object,
+        profile: Object,
+
+    },
+    setup(props) {
+        const user = computed(() => usePage().props?.value.auth.user);
+        const expert = computed(() => usePage().props?.value.auth.expert);
+
+        const {
+            PROFILE_PATH,
+            REQUEST_FINISHED,
+            USER_PROFILE_PATH,
+            REQUEST_CANCELED,
+        } = commonConst;
+
+        const profilePath = expert.value === null ? PROFILE_PATH : USER_PROFILE_PATH;
+
+        const { messages, chatroom, profile } = toRefs(props);
+
+        const newMessage = ref('')
+
+        //ブロードキャスト受信設定
+        const channel = Echo.private('message-channel.' + user.value?.id);
+        channel.listen('.message-event', function (data) {
+            messages.value.push(data.message);
+        });
+
+        //メッセージの送信
+        const submit = () => {
+            const params = {
+                chatroom_id: chatroom.value.id,
+                message: newMessage.value,
+                user_id: user.value?.id,
+                expert_id: expert.value?.id
+            }
+            axios.post(
+                route('message.update'),
+                params
+            ).then(res => {
+                messages.value.push(res.data)
+
+            }).catch(res => {
+                alert('データの登録に失敗しました。時間をおいてから再度お試しください')
+            })
+            newMessage.value = '';
+        }
+
+        //メッセージ入力時送信ボタンを表示
+        const isInput = ref(false);
+        watch(newMessage, () => {
+            isInput.value = newMessage.value.length !== 0;
+        })
+
+        //メッセージがログイン中の人物かを判定
+        const isLogin = (message) => {
+            //ログインがユーザーの場合
+            if (expert.value === null) {
+                return message?.user_id !== null;
+                //ログインが専門人材の場合
+            } else if (user.value === null) {
+                return message?.expert_id !== null;
+            }
+        }
+
+        //メッセージ下部にスクロール
+        const messageRef = ref(null);
+        onMounted(() => {
+            const target = messageRef.value;
+            target.scrollTop = target.scrollHeight
+
+            watch(messages.value, () => {
+                nextTick(() => target.scrollTop = target.scrollHeight)
+            })
+
+        })
+
+        //メッセージ送信可否
+        const canMessage = ref(true);
+        if (chatroom.value.request_status === REQUEST_CANCELED || chatroom.value.request_status === REQUEST_FINISHED) {
+            canMessage.value = false;
+        }
+
+        return {
+            newMessage,
+            messages,
+            submit,
+            isLogin,
+            messageRef,
+            profile,
+            profilePath,
+            canMessage,
+            isInput,
+        }
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+.user-right {
+    @apply items-end order-1
+}
+
+.user-message {
+    @apply bg-blue-600 text-white rounded-lg rounded-br-none
+}
+
+input:focus {
+    outline: none;
+    box-shadow: none;
+}
+</style>
--- resources/js/Pages/Users/Chatrooms/Show.vue
+++ resources/js/Pages/Users/Chatrooms/Show.vue
@@ -2,90 +2,11 @@
     <my-page-layout :isChatroom="true">
         <template #content>
             <div class="flex-1 p:2 sm:p-6 flex flex-col h-full">
-                <div class="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-                    <div class="flex items-center space-x-4">
-                        <img :src="PROFILE_PATH + expertProfile.profile_image" alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full">
-                        <div class="flex flex-col leading-tight">
-                            <div class="text-2xl mt-1 flex items-center">
-                                <span class="text-gray-700 mr-3">{{ expertProfile.nickname }}</span>
-                                <span class="text-green-500">
-                  <svg width="10" height="10">
-                     <circle cx="5" cy="5" r="5" fill="currentColor"></circle>
-                  </svg>
-               </span>
-                            </div>
-                            <span class="text-lg text-gray-600">{{ expertProfile.self_introduction }}</span>
-                        </div>
-                    </div>
-                    <div class="flex items-center space-x-2">
-                        <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                            </svg>
-                        </button>
-                        <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-                        </button>
-                        <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <div id="messages" class="flex flex-col space-y-4 p-3 overflow-y-scroll scrolling-touch h-screen" ref="messageRef">
-                    <div class="chat-message" v-for="(message, index) in messages" :key="index">
-                        <div class="flex items-end" :class="{'justify-end': isUser(message.user_id)}">
-                            <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start" :class="{'user-right': isUser(message.user_id)}">
-                                <div>
-                                    <span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600" :class="{'user-message': isUser(message.user_id)}">{{ message.message }}</span>
-                                </div>
-                            </div>
-                            <img v-if="!isUser(message.user_id)" :src="PROFILE_PATH + message.profile_image" alt="My profile" class="w-6 h-6 rounded-full order-1">
-                        </div>
-                    </div>
-                </div>
-                <div v-show="canMessage" class="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-                    <div class="relative flex">
-                         <span class="absolute inset-y-0 flex items-center">
-                            <button type="button" class="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
-                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path>
-                               </svg>
-                            </button>
-                         </span>
-                        <input v-model="newMessage" type="text" placeholder="Write Something" class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-full py-3">
-                        <div class="absolute right-0 items-center inset-y-0 hidden sm:flex">
-                            <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-                                </svg>
-                            </button>
-                            <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                </svg>
-                            </button>
-                            <button type="button" class="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                </svg>
-                            </button>
-                            <button @click="submit" type="button" class="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 transform rotate-90">
-                                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div v-show="!isRequesting" class="flex justify-center mt-4">
-                    <button @click="submitRequest(chatroom.request_status)" :class="{ 'opacity-25': requestForm.processing }" :disabled="requestForm.processing" class="expert-regular-btn mr-20">依頼する</button>
-                    <button @click="submitCancelConsultation" class="expert-outline-btn" :class="{ 'opacity-25': requestForm.processing }" :disabled="requestForm.processing">相談キャンセル</button>
-                </div>
+            <Chatroom :messages="messages" :chatroom="chatroom" :profile="expertProfile"/>
+            <div v-show="!isRequesting" class="flex justify-center mt-4">
+                <button @click="submitRequest(chatroom.request_status)" :class="{ 'opacity-25': requestForm.processing }" :disabled="requestForm.processing" class="expert-regular-btn mr-20">依頼する</button>
+                <button @click="submitCancelConsultation" class="expert-outline-btn" :class="{ 'opacity-25': requestForm.processing }" :disabled="requestForm.processing">相談キャンセル</button>
+            </div>
             </div>
         </template>
     </my-page-layout>
@@ -93,9 +14,10 @@
 
 <script>
 import MyPageLayout from '@/Layouts/Users/MyPageLayout'
-import { toRefs, ref, computed, onMounted, watch, nextTick } from 'vue'
-import { usePage, useForm } from "@inertiajs/inertia-vue3"
+import { toRefs, ref } from 'vue'
+import { useForm } from "@inertiajs/inertia-vue3"
 import { commonConst } from '@/Consts/commonConst'
+import Chatroom from '@/Layouts/Common/Chatroom'
 
 export default {
     name: "Show",
@@ -104,66 +26,18 @@ export default {
         chatroom: Object,
         expertProfile: Object,
     },
-    components: { MyPageLayout },
+    components: { Chatroom, MyPageLayout },
     setup(props) {
-        const user = computed(() => usePage().props?.value.auth.user);
-        const expert = computed(() => usePage().props?.value.auth.expert);
+
         const {
-            PROFILE_PATH,
             REQUEST_EXAMINATION,
             REQUEST,
             REQUEST_CANCELED,
-            REQUEST_FINISHED,
             CONSULTATION,
         } = commonConst;
+
         const { chatroom, messages, expertProfile } = toRefs(props);
-        const newMessage = ref('')
 
-        //ブロードキャスト受信設定
-        const channel = Echo.private('message-channel.' + user.value?.id);
-        channel.listen('.message-event', function (data) {
-            messages.value.push(data.message);
-        });
-
-        //メッセージの送信
-        const submit = () => {
-            const params = {
-                chatroom_id: chatroom.value.id,
-                message: newMessage.value,
-                user_id: user.value?.id,
-                expert_id: expert.value?.id
-            }
-            axios.post(
-                route('message.update'),
-                params
-            ).then(res => {
-                messages.value.push(res.data)
-
-            }).catch(res => {
-                alert('データの登録に失敗しました。時間をおいてから再度お試しください')
-            })
-            newMessage.value = '';
-        }
-
-        const isUser = (userId) => userId !== null
-
-        //メッセージ下部にスクロール
-        const messageRef = ref(null);
-        onMounted(() => {
-            const target = messageRef.value;
-            target.scrollTop = target.scrollHeight
-
-            watch(messages.value, () => {
-                nextTick(() => target.scrollTop = target.scrollHeight)
-            })
-
-        })
-
-        //メッセージ送信可否
-        const canMessage = ref(true);
-        if (chatroom.value.request_status === REQUEST_CANCELED || chatroom.value.request_status === REQUEST_FINISHED) {
-            canMessage.value = false;
-        }
 
         //依頼リクエスト・相談キャンセル
         const isRequesting = ref(false);
@@ -210,29 +84,17 @@ export default {
 
 
         return {
-            newMessage,
             messages,
-            submit,
-            isUser,
-            PROFILE_PATH,
-            messageRef,
             expertProfile,
             submitRequest,
             requestForm,
             isRequesting,
             submitCancelConsultation,
-            canMessage,
         }
     }
 }
 </script>
 
 <style scoped lang="scss">
-.user-right {
-    @apply items-end order-1
-}
 
-.user-message {
-    @apply bg-blue-600 text-white rounded-lg rounded-br-none
-}
 </style>
