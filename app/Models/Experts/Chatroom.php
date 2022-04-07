5c9439e4520306a86da32ae34cad36fe11d15822
tack-k <tyool9c@gmail.com>
2022-04-08 08:40:32 +0900

    feat:【E】チャットルーム一覧用のデータ取得方法を修正

--- app/Models/Experts/Chatroom.php
+++ app/Models/Experts/Chatroom.php
@@ -14,19 +14,21 @@ class Chatroom extends Model {
 
     public function scopeGetChatrooms($query, $expertId) {
         $latestMessage = \DB::table('messages')
-            ->selectRaw('expert_id, MAX(created_at) as latest_message_created_at')
-            ->groupBy('expert_id');
+            ->join('chatrooms as c', 'c.id', '=', 'messages.chatroom_id')
+            ->selectRaw('messages.user_id, MAX(messages.created_at) as latest_message_created_at')
+            ->where('c.expert_id', $expertId)
+            ->groupBy('messages.user_id');
 
-        $query->select('chatrooms.id as chatroom_id', 'consultation_status', 'request_status', 'chatrooms.created_at as c_created_at', 'chatrooms.updated_at as c_updated_at', 'request_finished_at', 'ep.nickname', 'ep.profile_image', 'm.message', 'm.created_at as m_created_at')
-            ->join('expert_profiles as ep', 'ep.id', '=', 'chatrooms.expert_id')
+        $query->select('chatrooms.id as chatroom_id', 'consultation_status', 'request_status', 'chatrooms.created_at as c_created_at', 'chatrooms.updated_at as c_updated_at', 'request_finished_at', 'up.nickname', 'up.profile_image', 'm.message', 'm.created_at as m_created_at')
+            ->join('user_profiles as up', 'up.user_id', '=', 'chatrooms.user_id')
             ->leftjoin('messages as m', function ($join) {
                 $join->on( 'm.chatroom_id', '=', 'chatrooms.id')
-                    ->whereNull('m.user_id');
+                    ->whereNull('m.expert_id');
             })
             ->joinSub($latestMessage, 'latest_message', function ($join) {
                 $join->on('m.created_at', '=', 'latest_message.latest_message_created_at')
-                    ->orWhereNull('m.user_id')
-                    ->whereNull('m.expert_id');
+                    ->orWhereNull('m.expert_id')
+                    ->whereNull('m.user_id');
             })
             ->where('chatrooms.expert_id', $expertId)
             ->distinct();
