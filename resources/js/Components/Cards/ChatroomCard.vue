57ca3d717a0bba1838cf97799eab628904423b11
tack-k <tyool9c@gmail.com>
2022-04-09 16:23:26 +0900

    style:【U】依頼のステータスを右上表示に変更

--- resources/js/Components/Cards/ChatroomCard.vue
+++ resources/js/Components/Cards/ChatroomCard.vue
@@ -2,27 +2,9 @@
     <!-- This is an example component -->
     <div class="w-full mx-auto user-hover" @click="linkChatroomShow(chatroom.chatroom_id)">
         <div class="max-w-sm p-2 h-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex justify-end px-4 pt-4">
-                <div :class="setConsultationColor(chatroom.consultation_status)" class="inline-flex items-center py-1 px-2 text-xs font-medium rounded">{{ chatroom.consultation_status_name }}</div>
-
-                <div id="dropdown"
-                     class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
-                    <ul class="py-1" aria-labelledby="dropdownButton">
-                        <li>
-                            <a href="#"
-                               class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export
-                                Data</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               class="block py-2 px-4 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                        </li>
-                    </ul>
-                </div>
+            <div class="flex justify-end px-4 py-4">
+                <div v-if="chatroom.consultation_status_name !== null" :class="setConsultationColor(chatroom.consultation_status)" class="inline-flex items-center py-1 px-2 text-xs font-medium rounded">{{ chatroom.consultation_status_name }}</div>
+                <div v-if="isShowRequestName(chatroom)" :class="setRequestColor(chatroom.request_status)" class="inline-flex items-center py-1 px-2 text-xs font-medium rounded">{{ chatroom.request_status_name }}</div>
             </div>
             <div class="flex flex-col items-center pb-10">
                 <div class="flex items-center">
@@ -32,9 +14,6 @@
                     </div>
                 </div>
                 <h3 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ chatroom.nickname }}</h3>
-                <div class="flex mt-4 space-x-3 lg:mt-6 mb-4">
-                    <div v-if="isShowRequestName(chatroom)" :class="setRequestColor(chatroom.request_status)" class="inline-flex items-center py-2 px-4 text-sm font-medium rounded">{{ chatroom.request_status_name }}</div>
-                </div>
                 <table>
                     <tbody class="text-sm text-gray-500 dark:text-gray-400">
                     <tr>
